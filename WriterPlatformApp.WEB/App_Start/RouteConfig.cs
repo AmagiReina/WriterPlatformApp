ca4c1b87fd7551fb331e3299fd31a2997fbb9371
AmagiReina <nupbayev93@gmail.com>
2020-03-09 15:15:22 +0600

    App Deploy Azure

--- WriterPlatformApp.WEB/App_Start/RouteConfig.cs
+++ WriterPlatformApp.WEB/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WriterPlatformApp.WEB
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Title", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
